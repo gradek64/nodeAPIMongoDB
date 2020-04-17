52a8c57ebeb68d8aa8cd88cb23eee3fc41795fec
gradek64 <gradek64@yahoo.com>
2020-04-17 11:41:43 +0100

    Mongodb access open to all ips for heroku

--- connectMongoDB.js
+++ connectMongoDB.js
@@ -19,7 +19,10 @@ var mongoose = require("mongoose");
     {username:test, password:test } and hosted on: mylab
   * 
   */
+//process.env.MONGODB_URI is used by heroku to connect to atlas.mongodb
+//make sure you have all connectiion open for atlas.mongodb tab NetworkAccess to all ips (not restricted Ips)
 const uri =
+  process.env.MONGODB_URI ||
   "mongodb+srv://gradek:grzesiek64@nodeapi-7qv5x.mongodb.net/test?retryWrites=true&w=majority";
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true }).then(
   () => {
